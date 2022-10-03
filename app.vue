8b8b0943b1979dfad30629606822f42a2cdc08fb
Andrzej Herok <andrzej@herok.pl>
2022-10-03 22:35:40 +0200

    Add Nuxt-image

--- app.vue
+++ app.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>hello</h1>
 
-    <img src="/astro.jpg"/>
+    <nuxt-img src="/astro.jpg" sizes="sm:100vw md:50vw lg:400px"/>
+    <h3>source: https://apod.nasa.gov/apod/ap220208.html</h3>
   </div>
 </template>
